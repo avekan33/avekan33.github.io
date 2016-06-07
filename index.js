d886b04ba1dbed8442de131922a753a945893643
avekan33 <avekan33@gmail.com>
2016-06-07 10:24:16 -0700

    Major revision, still some work to be done

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  //Scrolling Function
+  $('a[href^="#"]').click(function(e){
+    var target = $(this).attr('href');
+    var strip = target.slice(1);
+    var anchor = $("a[name='"+strip+"']");
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: anchor.offset().top
+      
+    },"slow");
+    
+  });
+
+});
